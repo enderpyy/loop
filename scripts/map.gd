6900c89654cb0a2e38d7a267ba09e669d6535f25
Komo-Blows <156711146+Komo-Blows@users.noreply.github.com>
2025-07-31 16:34:11 -0400

    map instances start

--- scripts/map.gd
+++ scripts/map.gd
@@ -8,3 +8,10 @@ var height: int = 27
 func _ready() -> void:
 	for i in range(width):
 		pass
+
+var levels = []
+
+func change_level():
+	get_child(0).position = Vector2(0, 0)
+	get_child(1).queue_free()
+	
